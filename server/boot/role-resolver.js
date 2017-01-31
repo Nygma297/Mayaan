5a51f390f62a3685932372a6ea89b0228fb48d7e
Aniket Munshi <Aniket Munshi>
2017-01-31 17:09:10 +0530

    Role resolver to allow acess to specific users(incomplete)

---
+++ server/boot/role-resolver.js
@@ -0,0 +1,33 @@
+module.exports = function(app) {
+  var role = app.models.role;
+
+  role.registerResolver('team', function(role, context, cb) {
+    if (context.modelName !== 'Category') {
+      return process.nextTick(() => cb(null, false));
+    }
+
+    var  userId = context.accessToken.userId;
+    if (! userId) {
+      return process.nextTick(() => cb(null, false));
+    }
+
+    context.model.findById(context.modelId, function(err, Category) {
+      if(err) return cb(err);
+      if(!Category) return cb(new Error("Category not found"));
+
+      //Check if AppUser can access of the Document associated with this Category
+      var Document = app.models.Document;
+      Document.count({
+        caegoryId: Category.categoryId
+      }, function(err, count) {
+        if (err) return cb(err);
+
+        if(count > 0){
+          return cb(null, true);
+        }else{
+          return cb(null, false);
+        }
+      });
+    });
+  });
+};
