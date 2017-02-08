7060433dc1e9435fe62bf7aa7cafffd8cac89367
Aniket Munshi <Aniket Munshi>
2017-02-08 13:42:45 +0530

    commited code

--- role-resolver.js
+++ role-resolver.js
@@ -1,28 +1,28 @@
-module.exports = function(app) {
-  var Role = app.models.Role;
+// module.exports = function(app) {
+//   var Role = app.models.Role;
 
-  Role.registerResolver('', function(role, context, cb) {
-    function reject() {
-      process.nextTick(function() {
-        cb(null, false);
-      });
-    }
+//   Role.registerResolver('', function(role, context, cb) {
+//     function reject() {
+//       process.nextTick(function() {
+//         cb(null, false);
+//       });
+//     }
 
-    if (context.modelName !== 'document') {
-      return reject();
-    }
+//     if (context.modelName !== 'document') {
+//       return reject();
+//     }
 
-    var userId = context.accessToken.userId;
-    if (!userId) {
-      return reject();
-    }
+//     var userId = context.accessToken.userId;
+//     if (!userId) {
+//       return reject();
+//     }
 
-    //user is in category table for the given document
-    context.model.findById(context.modelId, function(err, document) {
-      if (err || !document)
-        return reject();
+//     //user is in category table for the given document
+//     context.model.findById(context.modelId, function(err, document) {
+//       if (err || !document)
+//         return reject();
 
-      var userGroup = app.models.userGroup;
-    });
-  });
-};
+//       var userGroup = app.models.userGroup;
+//     });
+//   });
+// };
